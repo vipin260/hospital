fab1ce4fba9c6d36a713ba35596fdac0848dfb61
vipin260 <bwsvipin2022@gmail.com>
2022-08-01 13:40:38 +0530

    update condtion

--- src/Components/visiting/index.js
+++ src/Components/visiting/index.js
@@ -1074,7 +1074,7 @@ const history = () => {
                    
                     <Button sx={{ marginRight:'2%',background: '#e9e9e9',boxShadow: 'none',border: '1px solid #000',color: '#000!important',width:'100%'}}
                         variant="contained"
-                        className={active === i ? "con" : null }
+                        className={active === i && "con"  }
                         value={itemsname.id} 
                         name ={itemsname.name}
                         onClick={(items)=>clickOpd(items,i)}
