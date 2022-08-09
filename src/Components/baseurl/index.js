41a03379fdba71934788fe6439f7a9a3ae77bd3c
vipin260 <bwsvipin2022@gmail.com>
2022-08-09 10:26:17 +0530

    update

--- src/Components/baseurl/index.js
+++ src/Components/baseurl/index.js
@@ -1,6 +1,6 @@
 export const baseUrl = 'http://localhost/hotelManagement';
 
-export const linkUrl = 'https://3af3-223-178-210-167.in.ngrok.io/hospital_management/';
+// export const linkUrl = 'https://3af3-223-178-210-167.in.ngrok.io/hospital_management/';
 
-// export const linkUrl = 'http://localhost/hospital_management/';
+export const linkUrl = 'http://localhost/hospital_management/';
 
